f4e26f7d06a71f5e78a2ae9ada3d5359d124ba29
Vovo Yang <vovo.yang@gmail.com>
2016-06-20 20:02:50 +0000

    security with methods

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -42,12 +42,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
@@ -55,15 +50,11 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id);
   }
 }
 
